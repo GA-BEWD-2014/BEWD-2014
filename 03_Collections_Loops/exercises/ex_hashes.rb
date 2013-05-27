f66a4a59c524d9b29c7c56d4557ca217f825c784
Jessica T Skeete <jessicat@ga.co>
2013-05-27 19:36:01 -0400

    removing files that were deleted without git

--- 03_Collections_Loops/exercises/ex_hashes.rb
+++
@@ -1,9 +0,0 @@
-# Copy your solution from conditional_exercise.rb
-# Create an empty stories array. This will be used to hold all of our stories we capture.
-# Users enter the story as they do now, but it gets put in a hash instead. 
-# Update any reference to the story, upvotes and category string along the way
-# Break out the user input so the hash looks like the one below 
-# { title: "Monkeys thank mayor for flounder tooth necklace", category: "Teeth", upvotes: 1 }
-# Add this hash to an array called stories and print "Story: Monkeys thank mayor for flounder tooth necklace, Category: (Teeth), Current Upvotes: 1"
-# Through the stories array 
-# Test your cat, bacon, and food upvote upgrades work.
