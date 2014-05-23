2dcb3c4c744658563a918b11edfadd020a6dad6f
Ernst <ernsta@28cfe91ab2bd.ant.amazon.com>
2014-05-23 01:24:40 -0700

    modified two files

--- 02_Variables_Conditionals/starter_code/code_demo_boolean.rb
+++ 02_Variables_Conditionals/starter_code/code_demo_boolean.rb
@@ -2,15 +2,24 @@
 #Let the computer do the work for you!
 
 puts "Is 7 greater than 8?"
+puts 7>8
 
 puts "Is 8 x 77 greater than 600?"
+puts (8*77) > 600
 
 puts "Is 1 equal to (7 - 6)?"
+puts 1 == 7-6
 
 puts "Is 77 greater than 50 AND (88 / 3) less than 30?"
+puts 77>50 && 88/3 <300
 
 puts "Is the length of the word 'wheelbarrow' more than 10 characters long?"
+puts 'wheelbarrow'.length > 10
 
 puts "Are the amount of seconds in an hour greater than or equal to 3000?"
+puts 60*60 >= 3000
 
 puts "Does the word 'slaughter' include the word laughter?"
+puts 'slaughter'.include?('laughter') 
+
+#test
--- 02_Variables_Conditionals/starter_code/ex_teddit_conditional.rb
+++ 02_Variables_Conditionals/starter_code/ex_teddit_conditional.rb
@@ -5,14 +5,27 @@
 # Where you see comments (lines that begin with #) replace it with code so that the program works.
 
 def get_input
-  #Get input from the user.
+  gets.chomp.downcase
 end
 
-def calculate_upvotes(story, category)
+#upvotes = 1
+
+def calculate_upvotes(story, category, upvotes)
+#upvotes = 1
+
 	# Write code so that:
 		# If the Story is about cats multiply the upvotes by 5
+		if story.include?('cats')
+			upvotes = upvotes * 5
 		# If the Story is about bacon multiply the upvotes by 8
+		elsif story.include?('bacon')
+			upvotes = upvotes * 8
 		# If the Story is about Food it gets 3 times the upvotes.
+		elsif story.include?('food')
+			upvotes = upvotes * 3
+		else
+		 	upvotes = upvotes * 1
+		end	
 
 	#For example:
 	# "Cats frolic despite tuna shortage" should give you 5 times the upvotes!
@@ -23,5 +36,7 @@ puts "Please enter a News story:"
 story = get_input
 puts "Please give it a category:"
 category = get_input
-upvotes = calculate_upvotes(story, category)
-puts "New story added! #{story}, Category: #{category.capitalize}, Current Upvotes: #{upvotes}"
+puts "add upvotes"
+upvotes = get_input.to_i
+total_upvotes = calculate_upvotes(story, category, upvotes)
+puts "New story added! #{story}, Category: #{category.capitalize}, Current Upvotes: #{total_upvotes}"
