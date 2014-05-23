ee4b86f69ffc3b418833eaeac5138b7babbcaabc
Ernst <ernsta@28cfe91ab2bd.ant.amazon.com>
2014-05-22 19:09:55 -0700

    modified code_demo_rb

--- 02_Variables_Conditionals/starter_code/code_demo_methods.rb
+++ 02_Variables_Conditionals/starter_code/code_demo_methods.rb
@@ -5,43 +5,36 @@
 
 
 #1 - Let's say we wanted to perform the task of multiplying 22 by 44  
-###########
-###########
+22*44
 	
 #2 - Great, what if we wanted to multiply 22*4 again. If we had to type that out each time, it would be inefficient
 # So, let's try and save that code to a method...
-###########
-###########
-
-
+def multiply 
+	22*44
+end
 
 # NOTE: Running this does not return 968! How come?
 
 
 ##3 - Let's call that method, by simply typing out its name:
-###########
-###########
-
+multiply
 
 
 ##4 - Now, let's say we wanted to make it a little smarter. Our  method will take two parameters passed to it.
 # Let's name the parameters as variables x and y
 # and then multiply them together.
-###########
-###########
-
+def multiply(x,y)
+	x*y
+end	
 
 
 #5 - Now CALL that method:
-###########
-###########
+multiply
 
 
 
 #6 - Other People's methods. Two of the most common methods we will use throughout this class are puts and gets.
 # Let's start with puts...
-###########
-###########
 
 
 #7 - One more thing! - When you are calling a method with parameters, using the parenthesis () is OPTIONAL
