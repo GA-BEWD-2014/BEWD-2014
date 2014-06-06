691054947c08699457f450e77b63dfd91b8fde8e
Paul Mumma <paulmumma@Pauls-MacBook-Air-2.local>
2014-06-05 18:00:20 -0700

    Thursday commit

---
+++ building.rb
@@ -0,0 +1,10 @@
+class Building
+
+	attr_accessor :building_name, :building_address
+
+	def initialize(building_name, building_address)
+		@building_name = building_name
+		@building_address = building_address
+	end
+
+end
---
+++ person.rb
@@ -0,0 +1,9 @@
+class Renter
+	attr_accessor :name, :credit_score, :gender
+
+	def initialize (name, credit_score, gender)
+		@name = name
+		@credit_score = credit_score
+		@gender = gender
+	end
+end
